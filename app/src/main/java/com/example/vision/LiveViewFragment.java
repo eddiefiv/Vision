704854e98f0ba6a361937ae1d0687edc7bc55cbe
Waffwer <eddiefiv21@gmail.com>
2023-01-15 01:25:53 -0500

    Bottom Nav Bar and Create Fragments

---
+++ app/src/main/java/com/example/vision/LiveViewFragment.java
@@ -0,0 +1,31 @@
+package com.example.vision;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class LiveViewFragment extends Fragment {
+
+    public LiveViewFragment() {
+        // Required constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.live_view, container, false);
+
+        // Inflate the layout for this fragment
+        return view;
+    }
+}
--- app/src/main/java/com/example/vision/MainActivity.java
+++ app/src/main/java/com/example/vision/MainActivity.java
@@ -7,12 +7,17 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.widget.Toast;
 
 import com.example.vision.databinding.MainActivityBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static MainActivity instance;
 
     private static final int CAMERA_PERMISSION_CODE = 1;
 
@@ -23,13 +28,42 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         mainBinding = MainActivityBinding.inflate(getLayoutInflater());
         setContentView(mainBinding.getRoot());
+        replaceFragment(new LiveViewFragment());
+        instance = this;
 
-        mainBinding.permBtn.setOnClickListener(view -> {
-            checkCameraPermissions();
+        checkCameraPermissions();
+
+        mainBinding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            switch (item.getItemId()) {
+
+                case R.id.live_view:
+                    replaceFragment(new LiveViewFragment());
+                    break;
+                case R.id.save:
+                    replaceFragment(new SaveFragment());
+                    break;
+                case R.id.settings:
+                    replaceFragment(new SettingsFragment());
+                    break;
+            }
+
+            return true;
         });
     }
 
-    private void checkCameraPermissions() {
+    public static MainActivity getInstance() {
+        return instance;
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.main_frame_layout, fragment);
+        fragmentTransaction.commit();
+    }
+
+    public void checkCameraPermissions() {
         if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA}, CAMERA_PERMISSION_CODE);
         }
